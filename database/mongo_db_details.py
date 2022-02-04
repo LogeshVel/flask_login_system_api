d02ef05ffc969f6dd18fdefc475cc4655f28b2e3
LOGESH <logsak.12.34@gmail.com>
2022-02-04 21:16:10 +0530

    added dummy credentials

--- database/mongo_db_details.py
+++ database/mongo_db_details.py
@@ -1,5 +1,4 @@
 class Users:
-    SERVER_URI = "mongodb+srv://user_1:user_pass_api_python@cluster-one.lacs0.mongodb.net/myFirstDatabase?retryWrites" \
-                 "=true&w=majority&ssl=true&ssl_cert_reqs=CERT_NONE"
-    DATABASE_NAME = "user-database"
-    COLLECTION_NAME = "users-collection"
+    SERVER_URI = "<your_mongoDB_server_URI>"
+    DATABASE_NAME = "<you_database_name>"
+    COLLECTION_NAME = "<your_collection_name>"
